593d7fc048c6e421d31fac56934019ee05881862
Vladimir <vladimirplotnikov371@gmail.com>
2022-10-23 00:44:11 +0300

    user route back

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import { registerValidation, loginValidation } from './src/validations/validators.js'
+import{checkAuth} from './src/middlwares/auth.js'
+import cors from 'cors'
+import * as user from './src/routes/user.js';
 
 const app = express()
+app.use(express.json())
+app.use(cors());
 const PORT = 4444
 
 
@@ -13,6 +19,15 @@ async function start() {
     useUnifiedTopology: true,
   }
   ).then(() => console.log("success"))
+
+///user
+  app.post('/user/register', registerValidation, user.register)
+  app.post('/user/login', loginValidation, user.login)
+  app.get('/user/me', checkAuth, user.getMe);
+  app.put('/user/edit', checkAuth, registerValidation, user.edit)
+  app.delete('/user/remove', checkAuth, user.remove)
+
+///budget
   
   app.listen(PORT, () => {
    console.log("server started")
